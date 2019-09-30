a21f0159f0c6fd07d0f35e4f40524f9688bdcef8
RedRox13 <redrox94@gmail.com>
2019-09-30 12:48:23 +0300

    new

---
+++ src/app/favorites/favorites.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { FavoritesComponent } from './favorites/favorites.component';
+
+
+
+@NgModule({
+  declarations: [FavoritesComponent],
+  exports: [FavoritesComponent],
+  imports: [
+    CommonModule, SharedModule
+  ]
+})
+export class FavoritesModule { }
